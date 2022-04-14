641336f9fa91bfbb296bd66efb7e24ed1146e70d
hanzhang <hanzhangzhai@gmail.com>
2022-04-14 23:38:00 +0800

    rails article app created and set index to article

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+  # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
+
 end
