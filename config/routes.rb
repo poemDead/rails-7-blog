a2d3b3d369e1778b7d2c451d970d92f1aacf7ec5
hanzhang <hanzhangzhai@gmail.com>
2022-04-16 00:32:58 +0800

    rails crud create&new&show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # replace get to resources.Whenever we have such a combination of routes, controller actions, and views that work together to perform CRUD operations on an entity, we call that entity a resource.
+  resources :articles
+
   # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
 
 end
