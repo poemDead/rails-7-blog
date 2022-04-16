c1df1d75616227f96e6c7468e30bcb6b66d806ec
hanzhang <hanzhangzhai@gmail.com>
2022-04-16 12:28:33 +0800

    add comments model and function | use partial to make show.html simple to read

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   # get "/articles", to: "articles#index"
   # get "/articles/:id", to: "articles#show"
   # replace get to resources.Whenever we have such a combination of routes, controller actions, and views that work together to perform CRUD operations on an entity, we call that entity a resource.
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # The route above declares that GET /articles requests are mapped to the index action of ArticlesController.
 
